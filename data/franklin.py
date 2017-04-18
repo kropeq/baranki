f89b3664d330dbfe0bb295f686a1f6e51878a42d
pseroka <seroka.paulina@gmail.com>
2017-04-18 19:33:39 +0200

    Add files via upload

---
+++ data/franklin.py
@@ -0,0 +1,20 @@
+import pymongo
+from pymongo import MongoClient
+client = MongoClient()
+
+db = client['baranki']
+collection = db['names']
+
+pipeline = [
+	{ "$match": { "Name": "Franklin" } },
+	{ "$group": {
+		"_id": { "Year": "$Year" },
+		"Number": { "$sum": "$Count" }
+	}},
+	{ "$sort": { "_id.Year" : 1}}
+]
+ 
+zapytanie = db.names.aggregate(pipeline)
+
+for doc in zapytanie:
+   print(doc)
